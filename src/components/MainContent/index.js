ad69dcb43c750c8193b8b1de28134cd3cb7e35be
Sami Fares <djsamif@gmail.com>
2022-06-29 16:13:43 -0400

    single search working

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,18 +1,26 @@
 import styles from "./mainContent.module.css";
-
+import { useState } from "react";
+import { getCoinFromSearch } from "../../utils/api";
 function MainContent() {
+    const [searchedCoin, setsearhedCoin] = useState("");
+
+    const handleSubmit = () => {
+        //TODO - add loading state and loader
+        getCoinFromSearch(searchedCoin);
+    };
+
     return (
         <div className={styles.searchBarContainer}>
             <input
                 className={styles.searchBar}
                 type="text"
+                value={searchedCoin}
+                onChange={(e) => setsearhedCoin(e.target.value)}
                 placeholder="Search a coin"
             ></input>
-            <input
-                className={styles.submitButton}
-                type="submit"
-                value="Submit"
-            ></input>
+            <button onClick={handleSubmit} className={styles.submitButton}>
+                Submit
+            </button>
         </div>
     );
 }
